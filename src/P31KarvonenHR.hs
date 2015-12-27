13b5fb9962e0bd235657bcaff022fd3d7e48a35a
Charlie Harvey <charlie@mallory.hendred.net>
2015-12-27 08:17:08 +0000

    Finished off P31 implementation and tests

--- src/P31KarvonenHR.hs
+++ src/P31KarvonenHR.hs
@@ -1,9 +1,39 @@
 module P31KarvonenHR where
 import Control.Exception (catch, IOException)
 
+-- todo: GUI
+
 main :: IO ()
-main = undefined
+main = do
+    age <- promptN "Age: "
+    rhr <- promptN "Resting HR: "
+    putStrLn "Intensity | Rate"
+    putStrLn "----------------"
+    putStrLn $ concatMap (showStep age rhr) [55,60..95]
+
+showStep :: Double -> Double -> Double -> String
+showStep age rhr intensity = 
+    show intensity ++ "      | " ++ show thr ++ " bpm\n"
+  where
+    thr = targetHR age rhr intensity
+
+promptN :: String -> IO Double
+promptN m = do
+    putStr m
+    x <- readLn `catch` except
+    if x<0
+      then do
+        putStrLn "Numbers must be non-negative"
+        promptN m
+      else
+        return x
+  where
+    except e = do
+      putStrLn $ "Couldn't parse number. Error was: " ++ show (e::IOException)
+      promptN m
 
 targetHR :: Double -> Double -> Double -> Int
 targetHR age rhr intensity =
-    round (((220 - age - rhr) * intensity) + rhr)
+    round (((220 - age - rhr) * i) + rhr)
+  where
+    i = intensity/100
--- t/P31KarvonenHRSpec.hs
+++ t/P31KarvonenHRSpec.hs
@@ -1,7 +1,6 @@
 module P31KarvonenHRSpec (main,spec) where
 
 import Test.Hspec
-import Test.Hspec.QuickCheck
 import P31KarvonenHR hiding (main)
  
  
@@ -12,14 +11,14 @@ spec :: Spec
 spec = do
   describe "targetHR" $ do
     it "calculates the book example at 55% correctly" $ do
-      targetHR 22 65 0.55 `shouldBe` 138
+      targetHR 22 65 55 `shouldBe` 138
     it "calculates the book example at 60% correctly" $ do
-      targetHR 22 65 0.6 `shouldBe` 145
+      targetHR 22 65 60 `shouldBe` 145
     it "calculates the book example at 65% correctly" $ do
-      targetHR 22 65 0.65 `shouldBe` 151
+      targetHR 22 65 65 `shouldBe` 151
     it "calculates the book example at 85% correctly" $ do
-      targetHR 22 65 0.85 `shouldBe` 178
+      targetHR 22 65 85 `shouldBe` 178
     it "calculates the book example at 90% correctly" $ do
-      targetHR 22 65 0.9 `shouldBe` 185
+      targetHR 22 65 90 `shouldBe` 185
     it "calculates the book example at 95% correctly" $ do
-      targetHR 22 65 0.95 `shouldBe` 191
+      targetHR 22 65 95 `shouldBe` 191
