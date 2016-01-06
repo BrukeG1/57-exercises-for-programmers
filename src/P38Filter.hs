9ecdd16fc7d43ea33c147b4cd0d147c3f368350d
Charlie Harvey <charlie@mallory.hendred.net>
2016-01-06 16:28:43 +0000

    Initial work on P38 (filter) and some tests

---
+++ src/P38Filter.hs
@@ -0,0 +1,8 @@
+module P38Filter where
+
+filt :: (a -> Bool) -> [a] -> [a]
+filt _ [] = []
+filt p (x:xs) = if p x then x : filt p xs else filt p xs
+
+evn :: Integer -> Bool
+evn n = n `mod` 2 == 0
---
+++ t/P38FilterSpec.hs
@@ -0,0 +1,27 @@
+module P38FilterSpec (main,spec) where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import P38Filter hiding (main)
+ 
+ 
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "filt" $ do
+    it "filters even numbers when passed evn as an predicate and a list of numbers" $ do
+      filt evn [1,2,3,4] `shouldBe` [2,4]
+    it "returns an empty list when passed evn as an predicate and a list of numbers with no evens" $ do
+      filt evn [1,3,5,7,9] `shouldBe` []
+    prop "has the same behaviour as filter when called with evn as a predicate and some list xs" $
+      \xs -> filter evn xs == filt evn xs
+  describe "evn" $ do
+    it "identifies 2 as even" $ do
+      evn 2 `shouldBe` True
+    it "identifies 3 as odd" $ do
+      evn 3 `shouldBe` False
+    prop "has the same behaviour as even from the prelude" $
+          \x -> evn x == even x
+    
