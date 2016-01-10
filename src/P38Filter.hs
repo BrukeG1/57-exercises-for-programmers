21654198a00b876e0323299b5384a9a68c3ea4b0
Charlie Harvey <charlie@mallory.hendred.net>
2016-01-10 17:14:07 +0000

    Implemented filter in P38 as a fold, which seems a bit cleaner
    Also added a nice implementation of map as well

--- src/P38Filter.hs
+++ src/P38Filter.hs
@@ -7,17 +7,22 @@ main = do
     let evens = filt evn xs
     putStrLn $ "The even numbers are: " ++ show evens
 
-
 filt :: (a -> Bool) -> [a] -> [a]
-filt _ [] = []
-filt p (x:xs) = if p x then x : filt p xs else filt p xs
+filt p = foldr (\x xs -> if p x then x:xs else xs) []
+
+-- originally did:
+-- filt _ [] = []
+-- filt p (x:xs) = if p x then x : filt p xs else filt p xs
+
+-- not used, but cool
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
 
 evn :: Int -> Bool
 evn n = n `mod` 2 == 0
 
 od :: Int -> Bool
 od = not . evn
-
 
 promptXs :: String -> IO [Int]
 promptXs m = do
