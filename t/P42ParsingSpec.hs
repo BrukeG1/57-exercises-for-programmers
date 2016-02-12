886131c209a1680e91f6d72193442813ab2761ac
Charlie Harvey <charlie@mallory.hendred.net>
2016-02-12 17:48:17 +0000

    Tests for P42

--- t/P42ParsingSpec.hs
+++ t/P42ParsingSpec.hs
@@ -4,11 +4,26 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import P42Parsing hiding (main)
  
+testFile :: [String]
+testFile = [ "Ling,Mai,55900"
+           , "Johnson,Jim,56500"
+           , "Jones,Aaron,46000"
+           , "Jones,Chris,34500"
+           , "Swift,Geoffrey,14200"
+           , "Xiong,Fong,65000"
+           , "Zarnecki,Sabrina,51500"
+           ]
  
 main :: IO ()
 main = hspec spec
 
 spec = do
-  describe "main" $ do
-    it "is not implemented yet" $ 
-      pending
+  describe "lname" $ do
+    it "extracts 1st field from a list" $ do
+      lname ["a", "b", "c"] == "a"
+  describe "fname" $ do
+    it "extracts 2nd field from a list" $ do
+      fname ["a", "b", "c"] == "b"
+  describe "salary" $ do
+    it "extracts 3rd field from a list" $ do
+      salary ["a", "b", "c"] == "c"
