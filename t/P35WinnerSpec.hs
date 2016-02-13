6464058a611f7ae70380bc363198fa745a7a1dac
Charlie Harvey <charlie@mallory.hendred.net>
2016-02-13 13:42:07 +0000

    Got rid of pointless tests for P35

--- t/P35WinnerSpec.hs
+++ t/P35WinnerSpec.hs
@@ -8,17 +8,8 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "main" $ do
-    it "needs tests" $
-      pending -- dunno how to test monadic code yet...
-  describe "oneRound" $ do
-    it "needs tests" $
-      pending
   describe "deleteAt" $ do
     it "deletes first elt of a 2 elt list" $ do
       deleteAt 0 ["a","b"] `shouldBe` ["b"]
     it "deletes second elt of a 2 elt list" $ do
       deleteAt 1 ["a","b"] `shouldBe` ["a"]
-  describe "promptXs" $ do
-    it "needs tests" $
-      pending
