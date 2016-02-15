855fb2cf304dae0f9de414ff554e2adddc913832
Charlie Harvey <charlie@mallory.hendred.net>
2016-02-15 19:37:54 +0000

    Tidyup - redundant imports and do-s in Library and P14 tests

--- t/LibrarySpec.hs
+++ t/LibrarySpec.hs
@@ -1,54 +1,54 @@
 module LibrarySpec (main,spec) where
 
 import Test.Hspec
-import Test.Hspec.QuickCheck
-import Library hiding (main)
+import Library
 
 main :: IO ()
 main = hspec spec
 
+spec :: Spec
 spec = do
   describe "showD" $ do
-    it "rounds correctly for whole number floats" $ do
+    it "rounds correctly for whole number floats" $
       showD 9.0 `shouldBe` "9.00"
-    it "rounds correctly for non-whole number floats" $ do
+    it "rounds correctly for non-whole number floats" $
       showD 9.00012 `shouldBe` "9.00"
-    it "rounds correctly for .5" $ do
+    it "rounds correctly for .5" $
       showD 9.5 `shouldBe` "9.50"
-    it "rounds correctly for .05" $ do
+    it "rounds correctly for .05" $
       showD 9.05 `shouldBe` "9.05"
-    it "rounds correctly for .005" $ do
+    it "rounds correctly for .005" $
       showD 9.005 `shouldBe` "9.00"
-    it "rounds correctly for .006" $ do
+    it "rounds correctly for .006" $
       showD 9.006 `shouldBe` "9.01"
   describe "showD8" $ do
-    it "rounds and formats correctly for whole number floats" $ do
+    it "rounds and formats correctly for whole number floats" $
       showD8 9.0 `shouldBe` "    9.00"
-    it "rounds and formats correctly for non-whole number floats" $ do
+    it "rounds and formats correctly for non-whole number floats" $
       showD8 9.00012 `shouldBe` "    9.00"
-    it "rounds and formats correctly for .5" $ do
+    it "rounds and formats correctly for .5" $
       showD8 9.5 `shouldBe` "    9.50"
-    it "rounds and formats correctly for .05" $ do
+    it "rounds and formats correctly for .05" $
       showD8 9.05 `shouldBe` "    9.05"
-    it "rounds and formats correctly for .005" $ do
+    it "rounds and formats correctly for .005" $
       showD8 9.005 `shouldBe` "    9.00"
-    it "rounds correctly for .006" $ do
+    it "rounds correctly for .006" $
       showD8 9.006 `shouldBe` "    9.01"
   describe "uc" $ do
-    it "gives back an empty string if given an empty string" $ do
+    it "gives back an empty string if given an empty string" $
       uc "" `shouldBe` ""
-    it "gives back a capitalized string if given a lowercase string" $ do
+    it "gives back a capitalized string if given a lowercase string" $
       uc "eek" `shouldBe` "EEK"
-    it "gives back a capitalized string if given a mixed case string" $ do
+    it "gives back a capitalized string if given a mixed case string" $
       uc "eEk" `shouldBe` "EEK"
-    it "gives back a capitalized string if given an uppercase string" $ do
+    it "gives back a capitalized string if given an uppercase string" $
       uc "EEK" `shouldBe` "EEK"
   describe "lc" $ do
-    it "gives back an empty string if given an empty string" $ do
+    it "gives back an empty string if given an empty string" $
       lc "" `shouldBe` ""
-    it "gives back a lowercase string if given a lowercase string" $ do
+    it "gives back a lowercase string if given a lowercase string" $
       lc "eek" `shouldBe` "eek"
-    it "gives back a lowercase string if given a mixed case string" $ do
+    it "gives back a lowercase string if given a mixed case string" $
       lc "eEk" `shouldBe` "eek"
-    it "gives back a lowercase string if given an uppercase string" $ do
+    it "gives back a lowercase string if given an uppercase string" $
       lc "EEK" `shouldBe` "eek"
--- t/P14TaxCalcSpec.hs
+++ t/P14TaxCalcSpec.hs
@@ -1,16 +1,16 @@
 module P14TaxCalcSpec (main,spec) where
 
 import Test.Hspec
-import Test.Hspec.QuickCheck
 import P14TaxCalc hiding (main)
 
 main :: IO ()
 main = hspec spec
 
-spec = do
+spec :: Spec
+spec =
   describe "wisconsinTax" $ do
-    it "calculates and formats wisoconsin tax on $100 correctly" $ do
+    it "calculates and formats wisoconsin tax on $100 correctly" $
       wisconsinTax 100 `shouldBe` "Tax:                 5.50\nTotal:             105.50"
-    it "calculates and formats wisoconsin tax on $200 correctly" $ do
+    it "calculates and formats wisoconsin tax on $200 correctly" $
       wisconsinTax 200 `shouldBe` "Tax:                11.00\nTotal:             211.00"
 
