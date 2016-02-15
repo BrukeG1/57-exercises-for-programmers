569c384fe1b0c0042ce1fe71dd83979ae0b3565b
Charlie Harvey <charlie@mallory.hendred.net>
2016-02-15 19:39:57 +0000

    Tidyup P05 tests

--- t/P05MathsSpec.hs
+++ t/P05MathsSpec.hs
@@ -11,24 +11,25 @@ x, y :: Float
 x = 120
 y = 9
 
+spec :: Spec
 spec = do
   describe "plus" $ do
-    it "is the same as adding 2 floats and calling show" $ do
+    it "is the same as adding 2 floats and calling show" $
       x `plus` y `shouldBe` 129.0
     prop "has the property of commutativity" $
       \a b -> a `plus` b `shouldBe` b `plus` a
     prop "has the property of associativity [up to 3 decimal places] (a+b)+c==a+(b+c) " $
       \a b c -> ((a `plus` b) `plus` c) - (a `plus` (b `plus` c)) < 0.001
-  describe "minus" $ do
-    it "is the same as subtracting 1 floats from another and calling show on the result" $ do
+  describe "minus" $
+    it "is the same as subtracting 1 floats from another and calling show on the result" $
       x `minus` y `shouldBe` 111.0
   describe "divBy" $ do
-    it "is the same as dividing 1 float by another and calling show on the result" $ do
+    it "is the same as dividing 1 float by another and calling show on the result" $
       x `divBy` y `shouldBe` 13.333333
-    it "is the same as dividing 1 float by another and calling show on the result" $ do
+    it "is the same as dividing 1 float by another and calling show on the result" $
       y `divBy` x `shouldBe` 7.5e-2
   describe "times" $ do
-    it "is the same as multiplying 1 float by another and calling show on the result" $ do
+    it "is the same as multiplying 1 float by another and calling show on the result" $
       x `times` y `shouldBe` 1080.0
     prop "has the property of commutativity" $
       \a b -> a `times` b `shouldBe` b `times` a
