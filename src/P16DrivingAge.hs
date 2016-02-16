0534ff8158cead659e872c9a867d5d56773ec4cc
Charlie Harvey <charlie@mallory.hendred.net>
2016-02-16 14:21:22 +0000

    Refactor P16 driving age
    Use Library for prompt code
    Make testing easier by pulling drivingMsg out as a function
    Format tidy up
    Make into a module

--- src/P16DrivingAge.hs
+++ src/P16DrivingAge.hs
@@ -1,9 +1,14 @@
+module P16DrivingAge where
+
+-- TODO: tests!!
+
 import qualified Data.Map as M
+import Library
 
 type Country = String
 
 defaultDrivingAge :: Int
-defaultDrivingAge     = 17
+defaultDrivingAge = 17
 
 defaultDrivingCountry :: Country
 defaultDrivingCountry = "UK"
@@ -13,15 +18,19 @@ drivingAges = M.fromList [("UK",17), ("US",16)]
 
 main :: IO ()
 main = do
-    age <- promptN "How old are you: "
+    age <- promptNonNegInt "How old are you: "
     country <- promptS "Your country (eg. DE, US, etc default UK): "
-    putStrLn $ "You " ++ (if isOfDrivingAge country age then "are" else "are not") 
-               ++ " old enough to drive in " ++ countryStr country
+    putStrLn $ drivingMsg country age 
+
+drivingMsg :: Country -> Int -> String
+drivingMsg country age =
+       "You " ++ (if isOfDrivingAge country age then "are" else "are not")
+       ++ " old enough to drive in " ++ countryStr country
 
 countryStr :: Country -> String
 countryStr country =
     if knownCountry country 
-      then country 
+      then country
       else defaultDrivingCountry ++ ", don't know about " ++ country
 
 knownCountry :: Country -> Bool
@@ -33,19 +42,3 @@ isOfDrivingAge country age =
     age >= countryDrivingAge
   where
     countryDrivingAge = M.findWithDefault defaultDrivingAge country drivingAges
-
-promptS :: String -> IO String
-promptS m = do
-    putStr m
-    getLine
-
-promptN :: (Num a, Ord a, Read a) => String -> IO a
-promptN m = do
-    putStr m
-    x <- readLn -- could catch error here if we wanted to recover
-    if x<0
-      then do
-        putStrLn "Numbers must be non-negative"
-        promptN m
-      else
-        return x
