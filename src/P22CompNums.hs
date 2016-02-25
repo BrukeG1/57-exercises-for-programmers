f329a4a6dc5cbae7502b9c7d033d47cad21a42c8
Charlie Harvey <charlie@mallory.hendred.net>
2016-02-25 10:32:36 +0000

    Refactor P22, write tests for it

--- src/P22CompNums.hs
+++ src/P22CompNums.hs
@@ -1,42 +1,28 @@
+module P22CompNums where
+
+import Library
+
 main :: IO ()
 main = do
-    putStr "How many integers will you enter? "
-    x <- readLn
-    if x < 0
-      then error "You can't enter fewer than zero integers!"
-      else do 
-        is <- getUniqInts x []
-        putStrLn $ case maxInt is of
-                    Nothing -> "No largest integer found"
-                    Just n  -> "The largest integer was: " ++ show n
+    x <- promptNonNegInt "How many integers will you enter? "
+    is <- getUniqInts x []
+    putStrLn $ case maxInt is of
+                  Nothing -> "No largest integer found"
+                  Just n  -> "The largest integer was: " ++ show n
 
--- get a number of unique integers from the user
+-- Get some number of unique integers from the user
 getUniqInts :: Int -> [Int] -> IO [Int]
 getUniqInts 0 xs = return xs
 getUniqInts n xs = do
-  i <- prompt "Enter an unique integer: " xs
+  i <- promptUniq "Enter an unique integer: " xs
   getUniqInts (n-1) (i : xs)
 
--- A custom prompt implementation. Take a message and a list of already seen entries
--- and keep asking for numbers until you get one that is not in the list already
-prompt :: (Eq a, Read a) => String -> [a] -> IO a
-prompt m xs = do
-    putStr m
-    x <- readLn -- catch parse error to recover
-    if x `elem` xs
-      then do
-        putStrLn "Integers must be unique!"
-        prompt m xs
-      else
-        return x
-
 -- Makes calling max a bit nicer
-maxInt :: Ord a => [a] -> Maybe a
+maxInt ::  [Int] -> Maybe Int
 maxInt = max' Nothing
 
 -- Get the maximum from a list of Ords
-max' :: Ord a => Maybe a -> [a] -> Maybe a
-max' Nothing []        = Nothing
+max' :: (Ord a, Eq a) => Maybe a -> [a] -> Maybe a
 max' n []              = n
 max' Nothing (x:xs)    = max' (Just x) xs
 max' m@(Just n) (x:xs) = if x > n
---
+++ test/P22CompNumsSpec.hs
@@ -0,0 +1,28 @@
+module P22CompNumsSpec (main,spec) where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck((==>))
+import P22CompNums hiding (main)
+import Data.List(sort)
+
+emptyList :: [Int]
+emptyList = []
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    describe "max'" $ do
+      it "gives Nothing as the max of an empty list" $ do
+        max' Nothing emptyList `shouldBe` Nothing
+      it "gives 4 as the max of [4,1]" $ do
+        max' Nothing [4,1] `shouldBe` Just 4
+      it "gives 4 as the max of [1,4]" $ do
+        max' Nothing [1,4] `shouldBe` Just 4
+    describe "maxInt" $ do
+      prop "has the property that maxInt of list is the same as Just (last $ sort list)" $
+        \xs -> not (null xs) ==>
+                maxInt xs `shouldBe` Just (last $ sort xs)
+
