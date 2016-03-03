8af662a04118de54f2eda5a7f835150df332836b
Charlie Harvey <charlie@mallory.hendred.net>
2016-03-03 09:45:44 +0000

    Typo in case insensitive test for P44

--- test/P44ProductSearchSpec.hs
+++ test/P44ProductSearchSpec.hs
@@ -16,7 +16,7 @@ spec = do
       it "returns the catalogue entry for a widget, with part of the name" $ do
         findByName "Wid" `shouldReturn` [Product {name = "Widget", price = 25.0, quantity = 5}]
       it "returns the catalogue entry for a widget, with part of the name, case insensitively" $ do
-        findByName "Wid" `shouldReturn` [Product {name = "Widget", price = 25.0, quantity = 5}]
+        findByName "wid" `shouldReturn` [Product {name = "Widget", price = 25.0, quantity = 5}]
       it "returns an empty list for an unsuccessful search" $ do
         findByName "jkdajdksadksajkd" `shouldReturn` []
 
