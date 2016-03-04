de8fb49fecc10e96854e38a6a3e4e8ba8a6c4900
Charlie Harvey <charlie@mallory.hendred.net>
2016-03-04 07:56:21 +0000

    Tests for P43

--- test/P43WebsiteGenSpec.hs
+++ test/P43WebsiteGenSpec.hs
@@ -4,6 +4,8 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck((==>))
 import P43WebsiteGen hiding (main)
+import Data.List (isInfixOf)
+
 
 main :: IO ()
 main = hspec spec
@@ -11,5 +13,9 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "indexHTML" $ do
-      it "pends" pending
+      it "sets title correctly" $ do
+        "<title>mytitle" `isInfixOf` indexHTML meta
+      it "sets author correctly" $ do
+        "<meta name=\"author\" value=\"myauthor\">" `isInfixOf` indexHTML meta
 
+meta = Meta "mytitle" "myauthor"
