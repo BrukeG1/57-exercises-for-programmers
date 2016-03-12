d6b38e3666f6954e963496e36f9575cafd4ad475
Charlie Harvey <charlie@mallory.hendred.net>
2016-03-12 21:38:13 +0000

    Solution and partial tests for P48 completed

---
+++ src/P48Weather.hs
@@ -0,0 +1,128 @@
+{-# LANGUAGE OverloadedStrings #-}
+module P48Weather where
+
+import Control.Applicative (Const)
+import Control.Lens ((^.),(^?))
+import Data.Aeson.Lens (_String, _Number, _Integer, key, nth, AsValue)
+import Data.Aeson.Types (Value)
+import Data.ByteString.Lazy (ByteString)
+import Data.Monoid (First)
+import Data.Scientific (toRealFloat, Scientific)
+import Data.Text (unpack, Text)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Data.Time.Format (defaultTimeLocale,formatTime)
+import Library (promptS)
+import Network.Wreq (get, Response, responseBody)
+import PrivateConfig (weatherAPIKey)
+
+-- Might as well use our code from earlier for temp conversions (though
+-- the API can do this for us if required)
+import P18TempConverter (Temperature (Kelvin), toCelsius, toFarenheit)
+
+data CompassDirection = North
+                      | NorthNorthEast
+                      | NorthEast
+                      | EastNorthEast
+                      | East
+                      | EastSouthEast
+                      | SouthEast
+                      | SouthSouthEast
+                      | South
+                      | SouthSouthWest
+                      | SouthWest
+                      | WestSouthWest
+                      | West
+                      | WestNorthWest
+                      | NorthWest
+                      | NorthNorthWest
+                      deriving Show
+
+main :: IO ()
+main = do
+    p <- promptS "Where are you?: "
+    w <- weatherForPlace p
+    putStrLn $ "Current temperature is "
+             ++ (show . toFarenheit $ getTemp w)
+             ++ " ("
+             ++ (show . toCelsius $ getTemp w)
+             ++ ")"
+    putStrLn $ "Weather description: " ++ (unpack $ getDescription w)
+    putStrLn $ "Wind direction is " ++ (show $ getWindDirection w)
+    putStrLn $ "Wind speed is " ++ (show $ getWindSpeed w) ++ " meter/sec"
+    putStrLn $ "Air pressure is " ++ (show $ getPressure w) ++ " hPa"
+    putStrLn $ "Sun rises at " ++ getSunrise w
+    putStrLn $ "Sun sets at " ++ getSunset w
+    putStrLn $ "Humidity is " ++ (show $ getHumidity w) ++ "%"
+
+weatherForPlace :: String -> IO (Response ByteString)
+weatherForPlace place = get $ "http://api.openweathermap.org/data/2.5/weather?q="
+                              ++ place
+                              ++ "&appid="
+                              ++ weatherAPIKey
+
+-- Given a Response, a lens focus, a conversion function and the item name we're extracting
+-- return the item in some useful format or throw an error (with the unretrievable item name)
+getItem :: Response body0
+           -> ((a -> Const (First a) a) -> body0 -> Const (First a) body0)
+           -> (a -> t)
+           -> String
+           -> t
+getItem w l cf itm =
+    case t of
+      (Just n) -> cf n
+      Nothing  -> error $ "Could not retrieve " ++ itm
+  where
+    t = w ^? responseBody . l
+
+getTemp :: AsValue b => Response b -> Temperature
+getTemp w =
+    getItem w (key "main" . key "temp" . _Number) (Kelvin . toRealFloat) "temperature"
+
+getHumidity :: AsValue b => Response b -> Integer
+getHumidity w =
+    getItem w (key "main" . key "humidity" . _Number) round "humidity"
+
+getPressure :: AsValue b => Response b -> Integer
+getPressure w =
+    getItem w (key "main" . key "pressure" . _Number) round "pressure"
+
+getWindSpeed :: AsValue b => Response b -> Double
+getWindSpeed w =
+    getItem w (key "wind" . key "speed" . _Number) toRealFloat "wind speed"
+
+getWindDirection :: AsValue b => Response b -> CompassDirection
+getWindDirection w =
+    getItem w (key "wind" . key "deg" . _Number) (degToDirection . toRealFloat) "wind direction"
+
+degToDirection d | d >= 348.75 || d <=  11.25 = North
+                 | d >   11.25 && d <   33.75 = NorthNorthEast
+                 | d >=  33.75 && d <=  56.25 = NorthEast
+                 | d >   56.25 && d <   78.75 = EastNorthEast
+                 | d >=  78.75 && d <= 101.25 = East
+                 | d >  101.25 && d <  123.75 = EastSouthEast
+                 | d >= 123.75 && d <= 146.25 = SouthEast
+                 | d >  146.25 && d <  168.75 = SouthSouthEast
+                 | d >= 168.75 && d <= 191.25 = South
+                 | d >  191.25 && d <  213.75 = SouthSouthWest
+                 | d >= 213.75 && d <= 236.25 = SouthWest
+                 | d >  236.25 && d <  258.75 = WestSouthWest
+                 | d >= 258.75 && d <= 281.25 = West
+                 | d >  281.25 && d <  303.75 = WestNorthWest
+                 | d >= 303.75 && d <= 326.25 = NorthWest
+                 | d >  326.25 && d <  348.75 = NorthNorthWest
+                 | otherwise            = error $ "Bad wind direction: " ++ (show d)
+
+getDescription :: AsValue b => Response b -> Text
+getDescription w =
+    getItem w (key "weather" . nth 0 . key "description" . _String) id "description"
+
+getSunrise :: AsValue b => Response b -> String
+getSunrise w =
+    getItem w (key "sys" . key "sunrise" . _Integer) unixToDateString "sunrise"
+
+getSunset :: AsValue b => Response b -> String
+getSunset w =
+    getItem w (key "sys" . key "sunset" . _Integer) unixToDateString "sunset"
+
+unixToDateString =
+    formatTime defaultTimeLocale  "%Y-%m-%d %H:%M:%S %Z" . posixSecondsToUTCTime . fromInteger
---
+++ test/P48WeatherSpec.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE OverloadedStrings #-}
+module P48WeatherSpec (main,spec) where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck((==>))
+import P48Weather hiding (main)
+import Control.Lens ((^.),(^?))
+import Data.Aeson.Lens (_String, _Number, _Integer, key, nth, AsValue)
+import Data.Aeson.Types (Value)
+import Network.Wreq (get, Response, responseBody)
+import Data.Text (unpack)
+import P18TempConverter (Temperature (Kelvin), toCelsius, toFarenheit)
+
+
+
+
+main :: IO ()
+main = hspec spec
+
+mock = weatherForPlace "London, UK"
+
+spec :: Spec
+spec = do
+    describe "weatherForPlace" $ do
+      it "gets weather for the correct country" $ do
+        w' <-weatherForPlace "Oxford, UK"
+        (w' ^. responseBody . key "sys" . key "country" . _String) `shouldBe` "GB"
+    describe "weatherForPlace" $ do
+      it "gets weather for the correct place name" $ do
+        w' <-weatherForPlace "Oxford, UK"
+        (w' ^. responseBody . key "name" . _String) `shouldBe` "Oxford"
+    describe "weatherForPlace" $ do
+      it "gets weather for the correct id" $ do
+        w' <-weatherForPlace "Oxford, UK"
+        (w' ^? responseBody . key "id". _Integer) `shouldBe` Just 2640729
+    describe "getTemp" $ do
+      it "gets some reasonably sensible temperature i.e. > 200K (which is cold for anywhere. Even London)." $ do
+        m <- mock
+        let (Kelvin n) = getTemp m
+        n > 200 `shouldBe` True
+    describe "getHumidity" $ do
+      it "gets a number in range 0..100 for humidity" $ do
+        m <- mock
+        let n = getHumidity m
+        n >= 0 && n <= 100 `shouldBe` True
+    describe "getPressure" $ do
+      it "gets some number for air pressure" $ do
+        m <- mock
+        let n = getPressure m
+        n > 0 `shouldBe` True
+    describe "getWindSpeed" $ do
+      it "gets some number for wind speed" $ do
+        m <- mock
+        let n = getWindSpeed m
+        n >= 0 `shouldBe` True
+    describe "getWindDirection" $ do
+      it "pends" pending
+    describe "getDescription" $ do
+      it "gets some non-empty description string" $ do
+        m <- mock
+        let n = getDescription m
+        unpack n `shouldSatisfy` (not . null)
+    describe "getSunrise" $ do
+      it "gets some non-empty sunrise time string" $ do
+        m <- mock
+        let n = getSunrise m
+        n `shouldSatisfy` (not . null)
+    describe "getSunset" $ do
+      it "gets some non-empty sunset time string" $ do
+        m <- mock
+        let n = getSunset m
+        n `shouldSatisfy` (not . null)
+    describe "degToDirection" $ do
+      it "pends" pending
+    describe "unixToDateString" $ do
+      it "pends" pending
+
